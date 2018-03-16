5d11991ad540b13a01467cc161ab085d1869a12a
David Josse <david.josse@campus-numerique-in-the-alps.com>
2018-03-16 14:44:42 +0100

    cours et livrable module perso

---
+++ test_openclass/ScanTest.java
@@ -0,0 +1,16 @@
+
+
+public class ScanTest {
+
+
+    public static void main(String[] args){
+        int a = 1, b = 15;
+        while (a < b)
+        {
+            System.out.println("coucou " +a+ " fois !!");
+            a++;
+        }
+
+
+    }
+}
---
+++ test_openclass/Test.java
@@ -0,0 +1,28 @@
+public class Test {
+
+        public static void main(String[] args)
+
+        {
+
+            String[] tab = {"toto", "tata", "titi", "tete"};
+
+            parcourirTableau(tab);
+
+        }
+
+
+
+        static void parcourirTableau(String[] tabBis)
+
+        {
+
+            for(String str : tabBis)
+
+                System.out.println(str);
+
+        }
+
+        }
+
+
+
